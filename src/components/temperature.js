1a5c5568a14e093a70e250c4a2a16cf94ec87f44
Brandon Davis <49970067+Bdavis90@users.noreply.github.com>
2019-12-29 21:19:14 -0500

    added degree-section class to temperature component and styled the app

--- src/components/temperature.js
+++ src/components/temperature.js
@@ -5,7 +5,10 @@ class Temperature extends Component {
   render() {
     return (
       <div className="temperature">
-        <h2 className="degree">34</h2>
+        <div className="degree-section">
+          <h2 className="temperature-degree">34</h2>
+          <span>F</span>
+        </div>
         <div className="temperature-description">It's cold</div>
       </div>
     );
